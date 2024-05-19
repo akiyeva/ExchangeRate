dc4eec8001d27075f78ef93b26c4dbe633cf42a1
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-05-19 23:43:06 +0400

    Add project files.

---
+++ ExchangeRate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ExchangeRate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal azn;
+
+            string codeUsd = "USD";
+            decimal rateUsd = 1.7M;
+
+            string codeRub = "RUB";
+            decimal rateRub = 0.022M;
+
+            string codeTry = "TRY";
+            decimal rateTry = 0.23M;
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter command: ");
+                string command = Console.ReadLine();
+
+                if (command == "/show-recent-currency-rates")
+                {
+                    Console.WriteLine(codeUsd + ": " + rateUsd);
+                    Console.WriteLine(codeRub + ": " + rateRub);
+                    Console.WriteLine(codeTry + ": " + rateTry);
+
+                }
+            }
+
+
+        }
+    }
+}
